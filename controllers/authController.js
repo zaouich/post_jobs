ffc7997131e83ca9a8f88a975866a4ca5fb79f5c
bader zaouich <baderzaouichbz@example.com>
2023-01-02 21:27:50 +0100

    add authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const AppError = require("../utils/AppError")
 const jwt = require("jsonwebtoken")
 const catchAsync = require("../utils/CatchAsync")
 const User = require("../models/userModel")
+// authentication
 
 const checkLogin = catchAsync(async(req,res,next)=>{
     //1 check if the jwt is exicts
@@ -16,7 +17,17 @@ const checkLogin = catchAsync(async(req,res,next)=>{
     //5 check if the user doenst  change the password after generating the jwt 
     if(user.isChanged(verified.iat))return next( new AppError(401,"the password has been changed plase try to loging first"))
     // 6 validate and call next
+    req.user = user
     next()
 })
-
+// authorization
+const restractTo = (...roles)=>{
+    return (req,res,next)=>{
+        // check if the user is ...
+        if(! roles.includes(req.user.role)) return next(new AppError(401,"you are not autorised to do that"))
+        // if he is autorised call next
+        next()
+    }
+    
+}
 module.exports = checkLogin
